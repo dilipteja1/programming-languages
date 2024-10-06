eb93a1ff4eecafcaed14fede76aaead2f59e4532
dilipteja1 <tejadilip23898@gmail.com>
2024-10-06 12:34:21 -0400

    learnings

---
+++ Java/learnings/Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String[] args){
+        char k = 'a';
+        k++;
+        System.out.println(k); 
+        int a = 257;
+        byte b = (byte)a; 
+        System.out.println(b);
+    }
+}
